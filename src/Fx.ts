d66ac05f7c1dda68b3b8de8a1a0181ec61cb101a
deadlinecode <github@deadlinecode.de>
2023-07-25 16:51:06 +0200

    Added timeout because else Offline wouldn't work

--- src/Fx.ts
+++ src/Fx.ts
@@ -5,8 +5,16 @@ class Fx {
   padTime = (text: string | number) => ("0" + text).slice(-2);
 
   ping = async (url: string): Promise<number> => {
+    const controller = new AbortController();
+    const ts = setTimeout(() => controller.abort(), 5000);
     const start = new Date().getTime();
-    await fetch(url, { method: "HEAD", cache: "no-cache", mode: "no-cors" });
+    await fetch(url, {
+      method: "HEAD",
+      cache: "no-cache",
+      mode: "no-cors",
+      signal: controller.signal,
+    });
+    clearTimeout(ts);
     return new Date().getTime() - start;
   };
 
